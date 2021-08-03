5d0f18593960a629d2d638d553fe0b92d20806e2
kenny <kennycs@protonmail.com>
2021-08-03 14:23:58 -0400

    Created is_legacy function

--- legacy.py
+++ legacy.py
@@ -1,5 +1,23 @@
 import argparse
 import asyncio
+import aiohttp
+import json
+
+
+async def is_legacy(username: str) -> bool:
+    url = 'https://api.ashcon.app/mojang/v2/user{username}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                text = await response.text()
+                json_data = json.loads(text)
+                if 'legacy' in json_data:
+                    if json_data['legacy'] == 'true':
+                        return True
+    except Exception as e:
+        print(e)
+
+    return False
 
 
 async def main():
